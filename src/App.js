f8bdd88687214599b5c43a3d86f0c44b5e1e0b9e
Raymond <raymondjohnson@pursuit.org>
2023-06-11 13:10:36 -0400

    2nd commity

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Nav from "./components/Nav"
 import Episodes from './components/Episodes';
 import Characters from './components/Characters';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router> */}
         <Nav />
         <Routes>
-          {/* <Route path= "/" element = { <div>Home</div>}/> */}
           <Route path= "/" element = { <Home />}/>
           <Route path= "/episodes" element = {<Episodes/>}/>
           <Route path= "/characters" element = {<Characters/>}/>
         </Routes>
-        {/* <Footer /> */}
-      </Router>
+      
     </div>
   );
 }
